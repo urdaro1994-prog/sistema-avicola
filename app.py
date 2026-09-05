1b6c1704824b500b496b2db131ead2d4a45339c0
urdaro1994-prog <urdaro1994@gmail.com>
2026-09-04 22:14:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="App David",
+    page_icon="🥚",
+    layout="centered",
+    initial_sidebar_state="collapsed"
+)
+import streamlit as st
 import pandas as pd
 import psycopg2
 import os

d5a934be23cf9554803adf4464af5cd54e92cbb8
urdaro1994-prog <urdaro1994@gmail.com>
2026-09-07 12:49:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1210,10 +1210,10 @@ else:
                                                 str_app.warning("Remisión y registro de cartera eliminados.")
                                                 str_app.rerun()
 
-        elif str_app.session_state.sesion_principal == "📝 Registro Diario":
+elif str_app.session_state.sesion_principal == "📝 Registro Diario":
         str_app.subheader("📝 Registro Diario - Control Zootécnico (Hy-Line)")
         
-        AVES_INICIALES_GALPON_1 = 16000  # Puedes ajustar o hacer dinámico según el galpón si lo requieres
+        AVES_INICIALES_GALPON_1 = 16000  
         
         if rol_actual == "Invitado":
             str_app.warning("👀 Modo Invitado: Visualización de registros diarios.")

182abbd9ad88009e906384e357b29833e8b1c854
urdaro1994-prog <urdaro1994@gmail.com>
2026-09-07 20:49:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1153,7 +1153,7 @@ if str_app.session_state.seccion_activa == "📊 Utilidades":
 if str_app.session_state.sesion_principal == "📜 Registro Diario":
     str_app.subheader("📜 Módulo de Registro Diario (Edades, Mortalidad y Concentrado)")
     galpon_reg = str_app.selectbox("Seleccione el Galpón", ["Galpón 1", "Galpón 2", "Galpón 3"], key="galp_reg_sel")
-                tab_config, tab_diario, tab_historial = str_app.tabs(["⚙️ Configuración Inicial", "✍️ Registrar Día", "📊 Historial y PDF"])
+    tab_config, tab_diario, tab_historial = str_app.tabs(["⚙️ Configuración Inicial", "✍️ Registrar Día", "📊 Historial y PDF"])
         
         with tab_config:
             str_app.markdown("#### Configuración Inicial del Lote (Se ingresa una sola vez)")
